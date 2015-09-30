f7be572a14b5c8b2c37f58f8d2ea593d25be4618
Dijish <dijishuk@gmail.com>
2015-09-30 23:27:37 +0530

    merged two image,but size is not proper.

---
+++ merge_image3.php
@@ -0,0 +1,25 @@
+<?php
+$img_dest = imagecreatefrompng('test_cover1.png');
+$img_src_or = imagecreatefromjpeg('test_profile_picture1.jpg');
+$width=500;
+$height=250;
+$img_src_resize = imagecreatetruecolor( $width, $height );
+imagealphablending( $img_src_resize, false );
+imagesavealpha( $img_src_resize, true );
+imagecopyresampled( $img_src_resize, $img_src_or, //dst_image, src_image
+                    250, 0, //Coordenada x del punto de destino, Coordenada y del punto de destino
+                    0, 0, //Coordenada x del punto de origen, Coordenada y del punto de origen
+                    $width, $height, //Ancho del destino, Alto del destino
+                    imagesx($img_src_or), imagesy($img_src_or) ); //Ancho original, Alto original
+imagecopyresampled( $img_dest,$img_src_resize, //dst_image, src_image
+                    0, 0, //Coordenada x del punto de destino, Coordenada y del punto de destino
+                    0,0, //Coordenada x del punto de origen, Coordenada y del punto de origen
+                     //Ancho del destino, Alto del destino
+                    imagesx($img_dest), imagesy($img_dest),
+					imagesx($img_src_resize), imagesy($img_src_resize)); //Ancho original, Alto original
+
+header('Content-Type: image/png');
+imagepng($img_src_resize);
+imagedestroy($img_src_or);
+imagedestroy($img_dest);
+?>
