ed589f62b79e0dd0161879b282039a4a24c1a828
Dijish <dijishuk@gmail.com>
2015-10-18 19:06:52 +0530

    Update fb_config.php

--- fb_config.php
+++ fb_config.php
@@ -3,8 +3,8 @@
 
 // Loading application configuration into Facbook\Facebook service.
 $fb = new Facebook\Facebook([
-      'app_id' => '1712101509019695',
-      'app_secret' => '38ecfe39ef76105b07724794ce7916de',
+      'app_id' => '',
+      'app_secret' => '',
       'default_graph_version' => 'v2.4',
 ]);
 ?>
